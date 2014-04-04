694a1b4ba8587df20f281ddd0834d8c8e54c1ae3
Alexandre Paz <erzapito@gmail.com>
2014-04-04 19:02:32 +0200

    initial version

---
+++ server/rompe_silencio/protected/models/User.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+ * This is the model class for table "User".
+ *
+ * The followings are the available columns in table 'User':
+ * @property integer $Id
+ * @property string $username
+ * @property string $passwordhash
+ * @property string $name
+ * @property string $email
+ */
+class User extends CActiveRecord
+{
+	
+	public $password;
+	
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'User';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('username, passwordhash, name, email', 'required'),
+			array('username, passwordhash, name, email', 'length', 'max'=>45),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('Id, username, passwordhash, name, email', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'Id' => 'Id',
+			'username' => 'Username',
+			'passwordhash' => 'Passwordhash',
+			'name' => 'Name',
+			'email' => 'Email',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('Id',$this->Id);
+
+		$criteria->compare('username',$this->username,true);
+
+		$criteria->compare('passwordhash',$this->passwordhash,true);
+
+		$criteria->compare('name',$this->name,true);
+
+		$criteria->compare('email',$this->email,true);
+
+		return new CActiveDataProvider('User', array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @return User the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+	
+	public function beforeSave() {
+		if (!empty($this->password)) {
+			$this->passwordhash = self::hashPassword($this->password);
+		}
+		return parent::beforeSave();
+	}
+	
+	public static function hashPassword($password)
+	{
+		return crypt($password, self::generateSalt());
+	}
+	
+	public static function generateSalt(){
+		$staticsalt = 'rompe_tu_silencio_hashtag_2014';
+		$random = md5(uniqid(mt_rand(), true));
+		$salt = hash('sha512',$random.microtime().$staticsalt);
+		return '$2a$12$'.$salt;
+	}
+	
+	public function validatePassword($password)
+	{
+		return crypt($password,$this->passwordhash)===$this->passwordhash;
+	}
+}
---
+++ server/rompe_silencio/protected/views/user/_form.php
@@ -0,0 +1,42 @@
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'user-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'username'); ?>
+		<?php echo $form->textField($model,'username',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'username'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'passwordhash'); ?>
+		<?php echo $form->textField($model,'passwordhash',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'passwordhash'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'name'); ?>
+		<?php echo $form->textField($model,'name',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
