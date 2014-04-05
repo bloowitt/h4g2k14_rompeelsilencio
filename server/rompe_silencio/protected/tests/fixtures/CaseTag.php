6440d0ac019e717bad96ca1ce8f2a233c53a20bc
Alexandre Paz <erzapito@gmail.com>
2014-04-05 13:43:12 +0200

    Added CaseTag model

---
+++ server/rompe_silencio/protected/tests/fixtures/CaseTag.php
@@ -0,0 +1,14 @@
+<?php
+
+return array(
+	/*
+	'sample1'=>array(
+		'CaseId' => '',
+		'TagId' => '',
+	),
+	'sample2'=>array(
+		'CaseId' => '',
+		'TagId' => '',
+	),
+	*/
+);
---
+++ server/rompe_silencio/protected/tests/unit/CaseTagTest.php
@@ -0,0 +1,13 @@
+<?php
+
+class CaseTagTest extends CDbTestCase
+{
+	public $fixtures=array(
+		'caseTags'=>'CaseTag',
+	);
+
+	public function testCreate()
+	{
+
+	}
+}
